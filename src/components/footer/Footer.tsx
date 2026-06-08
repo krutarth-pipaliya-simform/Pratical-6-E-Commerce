ad6d5d27574c97882a21cd27d34ae92f0e543aba
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-06-08 16:42:36 +0530

    feat: add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,11 @@
 export const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="flex flex-col bg-[#0360F4] text-white p-4">
+            <div className="text-center">© 1996-2026, QuickCart.com, Inc. or its affiliates</div>
+
+            <a className="text-center" href="nowhere.com">
+                Contact Us
+            </a>
+        </footer>
+    );
 };
