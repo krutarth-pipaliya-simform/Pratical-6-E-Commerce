92d3b7826b93c8bfc09a949ec6237efd6ab9bb83
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-06-09 17:06:45 +0530

    Update src/components/Footer/Footer.tsx as per Jenith's suggestion.
    Co-authored-by: Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
         <footer className="flex flex-col bg-[#0360F4] text-white p-4">
             <div className="text-center">© 1996-2026, QuickCart.com, Inc. or its affiliates</div>
 
-            <a className="text-center" href="nowhere.com">
+            <a className="text-center" href="#">
                 Contact Us
             </a>
         </footer>
