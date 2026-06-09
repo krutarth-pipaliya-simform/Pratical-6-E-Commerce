be6e5ab33edc1694752bcf9dbe76d5a34d25bd6c
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-06-09 11:53:49 +0530

    feat: add product style

--- src/components/products/productsList/products/Products.tsx
+++ src/components/products/productsList/products/Products.tsx
@@ -4,7 +4,7 @@ import { Product } from "./Product";
 export const Products = () => {
     const { products } = useProductsContext();
     return (
-        <ul>
+        <ul className="p-4 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {products.map((product) => (
                 <Product key={product.id} product={product} />
             ))}
