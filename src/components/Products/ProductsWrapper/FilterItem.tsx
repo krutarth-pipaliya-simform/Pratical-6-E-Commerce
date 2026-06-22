f5471c9dffcd24353eb7121050edec0063f75e51
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-06-22 15:01:54 +0530

    chore: make separate handler function

--- src/components/Products/ProductsWrapper/FilterItem.tsx
+++ src/components/Products/ProductsWrapper/FilterItem.tsx
@@ -1,34 +1,38 @@
+import type { ChangeEvent } from "react";
 import { useSearchParams } from "react-router-dom";
 
 export const FilterItem = ({ category }: { category: string }) => {
     const [params, setParams] = useSearchParams();
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.checked) {
+            setParams((prev) => {
+                prev.append("categories", e.target.value);
+                return prev;
+            });
+        } else {
+            setParams((prev) => {
+                const params = new URLSearchParams(prev);
+
+                const categories = params
+                    .getAll("categories")
+                    .filter((category) => category !== e.target.value);
+
+                params.delete("categories");
+
+                categories.forEach((category) => {
+                    params.append("categories", category);
+                });
+
+                return params;
+            });
+        }
+    };
+
     return (
         <li>
             <input
-                onChange={(e) => {
-                    if (e.target.checked) {
-                        setParams((prev) => {
-                            prev.append("categories", e.target.value);
-                            return prev;
-                        });
-                    } else {
-                        setParams((prev) => {
-                            const params = new URLSearchParams(prev);
-
-                            const categories = params
-                                .getAll("categories")
-                                .filter((category) => category !== e.target.value);
-
-                            params.delete("categories");
-
-                            categories.forEach((category) => {
-                                params.append("categories", category);
-                            });
-
-                            return params;
-                        });
-                    }
-                }}
+                onChange={handleChange}
                 type="checkbox"
                 id={category}
                 name={category}
